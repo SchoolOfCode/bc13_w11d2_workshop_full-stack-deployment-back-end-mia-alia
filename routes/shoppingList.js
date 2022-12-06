7894919634f4b174b9d1fb0fb2f977c9ec1d9605
aliafarhana88 <alia.abdulghaffar@gmail.com>
2022-12-06 13:22:35 +0000

    bonus task - adding POST request to backend

--- routes/shoppingList.js
+++ routes/shoppingList.js
@@ -1,18 +1,29 @@
-import express from "express";
-import { getShoppingList, postListItem } from "../models/shoppingList.js";
+import express from 'express';
+import {
+	getShoppingList,
+	postListItem,
+	patchListItem,
+} from '../models/shoppingList.js';
 
 const router = express.Router();
 
 /* GET shopping list. */
-router.get("/", async (req, res) => {
-  const data = await getShoppingList();
-  res.json({ success: true, payload: data });
+router.get('/', async (req, res) => {
+	const data = await getShoppingList();
+	res.json({ success: true, payload: data });
 });
 
-router.post("/", async (req, res) => {
-  const { listItem } = req.body;
-  const result = await postListItem(listItem);
-  res.status(201).json({ success: true, payload: result });
+router.post('/', async (req, res) => {
+	const { listItem } = req.body;
+	const result = await postListItem(listItem);
+	res.status(201).json({ success: true, payload: result });
+});
+
+router.patch('/:id', async (req, res) => {
+	const id = req.params.id;
+	const completed = req.body;
+	const result = await patchListItem(id, completed);
+	res.status(200).json({ success: true, payload: result });
 });
 
 export default router;
